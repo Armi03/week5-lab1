fb137b8034ef1f3ecc9842ac8518d1206614e560
Armi Amipara <Armi.Amipara@metropolia.fi>
2024-10-15 21:04:16 +0300

    Session 5 Activity-1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hello from './Hello';
+import Bye from './Bye';
+
+// App.jsx
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Bye />
+    </div>
+  );
+}
+
+export default App;
